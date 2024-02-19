5e4ef8fef71b9b024a9a008369441293da4c6fe7
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-20 01:40:57 +0500

    bux fixed

--- src/Contants.jsx
+++ src/Contants.jsx
@@ -1,22 +1,24 @@
-import { Bookmark, Insure, Manage } from "./assets/images";
+import Manage from "./assets/images/Manage.svg";
+import Bookmark from "./assets/images/Bookmark.svg";
+import Insure from "./assets/images/Insure.svg";
 
 export const Projects_list = [
 	{
 		id: 1,
 		title: "Manage",
 		desc: "This project required me to build a fully responsive landing page to the designs provided. I used HTML5 for the structure of the site, Less for pixel-perfect, and JavaScript for opening and closing menus. For interactivity, used some libraries. They are AOS Slick.JS",
-		Img: (props) => <Manage {...props} />,
+		img: Manage,
 	},
 	{
 		id: 2,
 		title: "Bookmark",
 		desc: "Implementing responsive design principles along with tools like TailwindCSS and Slick.JS can greatly enhance the user experience on websites, especially when incorporating features such as accordions and leveraging UI frameworks like Daisy UI.",
-		Img: (props) => <Bookmark {...props} />,
+		img: Bookmark,
 	},
 	{
 		id: 3,
 		title: "Insure",
 		desc: "By utilizing responsive design techniques and SASS/SCSS for streamlined styling, websites can seamlessly adapt to various screen sizes while maintaining consistency in design elements and decorations, including the integration of high-resolution Retina images for enhanced visual appeal.",
-		Img: (props) => <Insure {...props} />,
+		img: Insure,
 	},
 ];
--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,5 +1,10 @@
 import "./portfolioItem.less";
 
-export const PortfolioItem = ({ title, Img, desc }) => {
-	return <li className="list__main-list-item">{<Img className="list__main-list-img" />}</li>;
+export const PortfolioItem = ({ title, img, desc }) => {
+	return (
+		<li className="list__main-list-item">
+			<img src={img} className="list__main-list-img" alt="" />
+			<h3 className="list__main-list-title">{title}</h3>
+		</li>
+	);
 };
