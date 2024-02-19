cab4084d964d25ba8176ffcafab1c4306308c2b5
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-20 01:56:12 +0500

    bug realy fixed

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -4,7 +4,9 @@ export const PortfolioItem = ({ title, img, desc }) => {
 	return (
 		<li className="list__main-list-item">
 			<img src={img} className="list__main-list-img" alt="" />
-			<h3 className="list__main-list-title">{title}</h3>
+			<div className="list__main-list-content">
+				<h3 className="list__main-list-title">{title}</h3>
+			</div>
 		</li>
 	);
 };
